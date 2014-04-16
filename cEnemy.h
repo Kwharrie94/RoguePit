879506851e72e58256cdf93f5e377f46c1329f86
Kwharrie94 <Kerr_68gt500@hotmail.co.uk>
2014-04-16 21:49:35 +0100

    First Commit
    Have a character in a room.
    Have everything set up to allow for spawning of monsters, just need to
    add it.

---
+++ cEnemy.h
@@ -0,0 +1,19 @@
+
+#ifndef _CENEMY_H
+#define _CENEMY_H
+#include "cSprite.h"
+
+class cEnemy : public cSprite
+{
+private:
+	int mScore;
+	eDirection mDirection;
+
+public:
+	cEnemy();
+	cEnemy(D3DXVECTOR3 sPosition, LPDIRECT3DDEVICE9 pd3dDevice, LPCSTR theFilename); // Constructor
+	void update(float deltaTime);					// update the balloons position based on the translation
+	eDirection getDirection();      // Rturns the current direction of the sprite
+	void setDirection(eDirection theDirection); // Sets the current direction for the sprite.
+};
+#endif
